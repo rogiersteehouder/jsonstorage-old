86cae3e065e256d0def7db7479d614e3d84deda5
Rogier Steehouder (dr102654a) <rsteehouder@rocvantwente.nl>
2021-12-14 14:00:54 +0100

    Added history to the database and support for sync

--- jsonstorage.py
+++ jsonstorage.py
@@ -32,7 +32,8 @@ except:
 # Optional for yaml config
 try:
     from ruamel.yaml import YAML
-    yaml = YAML(typ='safe', pure=True)
+    yaml = YAML(pure=True)
+    yaml.default_flow_style = False
 except:
     yaml = None
 
@@ -75,72 +76,63 @@ class Config:
     def __parent(self, keys, set_default=False):
         """Get the parent dict for a compound key"""
         p = self._config
-        for k in keys[:-1]:
-            try:
+        if set_default:
+            for k in keys[:-1]:
+                p = p.setdefault(k, {})
+        else:
+            for k in keys[:-1]:
                 p = p[k]
-            except KeyError:
-                if set_default:
-                    p[k] = p = {}
-                else:
-                    return None
         return p
 
-    def get(self, key, default=None, set_default=False):
+    def __getitem__(self, key):
         """Get a config value"""
         keys = key.split('.')
-        p = self.__parent(keys, set_default)
-        if p is None:
-            return default
         try:
+            p = self.__parent(keys)
             return p[keys[-1]]
         except KeyError:
-            if set_default:
-                p[keys[-1]] = default
-            return default
+            raise KeyError(key)
 
-    def set(self, key, val):
+    def __setitem__(self, key, val):
         """Set a config value"""
         keys = key.split('.')
         p = self.__parent(keys, set_default=True)
         p[keys[-1]] = val
 
-    def delete(self, key):
+    def __delitem__(self, key):
         """Remove a config value
         
         This may leave empty dicts behind.
         """
         keys = key.split('.')
-        p = self.__parent(keys)
-        if p is not None:
+        try:
+            p = self.__parent(keys)
             del p[keys[-1]]
-
-    def __getitem__(self, key):
-        """Get a config value"""
-        val = self.get(key, default=...)
-        if val == ...:
+        except KeyError:
             raise KeyError(key)
-        return val
 
-    def __setitem__(self, key, val):
-        """Set a config value"""
-        self.set(key, val)
-
-    def __delitem__(self, key):
-        """Remove a config value"""
-        self.delete(key)
+    def get(self, key, default=None, set_default=False):
+        """Get a config value"""
+        try:
+            return self[key]
+        except KeyError:
+            if set_default:
+                p[key] = default
+            return default
 
 
 #####
 # Basic authentication with password only
 #####
 class Security:
+    context = CryptContext(['pbkdf2_sha256'])
+
     """Basic single user authentication for FastAPI
     
     Use a callback function to save a changed password hash when needed.
     Add security.valid_password as a dependency.
     """
     def __init__(self, hash: str, *, period: int = 0, new_hash: callable = None):
-        self.context = CryptContext(['pbkdf2_sha256'])
         self.hash = hash
         self.new_hash = new_hash
 
@@ -199,29 +191,36 @@ class JSONStorage:
 
     def get_list(self,
         like: str = Query(None, description="Filter with sql 'like' syntax", example='%abc%'),
-        glob: str = Query(None, description="Filter with 'glob' syntax", example='\*abc\*')
+        glob: str = Query(None, description="Filter with 'glob' syntax", example='\*abc\*'),
+        sync: bool = Query(False, description="Output sync format with update date/time"),
+        cleanup: bool = Query(False, description="Do cleanup of database first")
     ):
         """List stored json objects"""
         criteria = []
         if like is not None:
-            criteria.append("name like :l")
+            criteria.append("s.name like :l")
         if glob is not None:
-            criteria.append("name glob :g")
+            criteria.append("s.name glob :g")
+        crit = ""
+        if criteria:
+            crit = "and {}".format(" and ".join(criteria))
 
         with self._db_connect() as conn:
-            crit = ""
-            if criteria:
-                crit = "where {}".format(" and ".join(criteria))
-
             cur = conn.cursor()
-            cur.execute("select name from storage {} order by name".format(crit), {'l': like, 'g': glob})
-            items = [row['name'] for row in cur.fetchall()]
+            if cleanup:
+                cur.execute("with old_stuff as (select s.name, s.effdt from storage s where s.effdt < datetime('now', '-1 years') {} and (s.status = 'I' or exists (select 1 from storage where name = s.name and effdt > s.effdt))) delete from storage where exists (select 1 from old_stuff where name = storage.name and effdt = storage.effdt)".format(crit))
+
+            if sync:
+                cur.execute("select s.name, s.effdt, s.status from storage s order by s.name, s.effdt".format(crit), {'l': like, 'g': glob})
+                items = [{ 'name': row['name'], 'effdt': datetime.datetime.fromisoformat(row['effdt']), 'status': row['status']} for row in cur.fetchall()]
+            else:
+                cur.execute("select s.name from storage s where not exists (select 1 from storage where name = s.name and effdt > s.effdt) and s.status = 'A' {} order by s.name".format(crit), {'l': like, 'g': glob})
+                items = [row['name'] for row in cur.fetchall()]
             cur.close()
 
         return items
     get_list.route = '/'
     get_list.route_params = {
-        'response_model': List[str],
         'responses': {
                 status.HTTP_200_OK: {
                     'description': 'List of json object ids',
@@ -232,6 +231,7 @@ class JSONStorage:
                     }
                 }
             },
+        'name': 'storage.list',
         'summary': 'List available json objects',
         'description': TrueString()
     }
@@ -242,7 +242,7 @@ class JSONStorage:
         """Retrieve a stored json object"""
         with self._db_connect() as conn:
             cur = conn.cursor()
-            cur.execute("select content from storage where name = :p", {'p': id})
+            cur.execute("select s.content from storage s where s.name = :p and not exists (select 1 from storage where name = s.name and effdt > s.effdt) and s.status = 'A'", {'p': id})
             row = cur.fetchone()
             cur.close()
         if row is None:
@@ -254,6 +254,7 @@ class JSONStorage:
             status.HTTP_200_OK: RESPONSE_JSON,
             **responses(status.HTTP_404_NOT_FOUND)
         },
+        'name': 'storage.get',
         'summary': 'Retrieve a json object',
         'description': TrueString(),
         'response_description': 'The stored json object'
@@ -266,7 +267,7 @@ class JSONStorage:
         """Store a json object"""
         with self._db_connect() as conn:
             cur = conn.cursor()
-            cur.execute("insert or replace into storage values(:p, :c)", {'p': id, 'c': json.dumps(content)})
+            cur.execute("insert into storage values(:p, CURRENT_TIMESTAMP, 'A', :c)", {'p': id, 'c': json.dumps(content)})
             cur.close()
         return content
     put_json.route = '/{id:path}'
@@ -276,6 +277,7 @@ class JSONStorage:
             status.HTTP_201_CREATED: RESPONSE_JSON,
             **responses(status.HTTP_404_NOT_FOUND)
         },
+        'name': 'storage.put',
         'summary': 'Store a json object',
         'description': TrueString(),
         'response_description': 'The stored json object'
@@ -292,7 +294,7 @@ class JSONStorage:
 
         with self._db_connect() as conn:
             cur = conn.cursor()
-            cur.execute("select content from storage where name = :p", {'p': id})
+            cur.execute("select s.content from storage s where s.name = :p and not exists (select 1 from storage where name = s.name and effdt > s.effdt) and s.status = 'A'", {'p': id})
             row = cur.fetchone()
             if row is None:
                 raise HTTPException(status.HTTP_404_NOT_FOUND)
@@ -300,7 +302,7 @@ class JSONStorage:
             json_orig = json.loads(row['content'])
             json_new = jsonpatch.apply_patch(json_orig, patch)
 
-            cur.execute("insert or replace into storage values(:p, :c)", {'p': id, 'c': json.dumps(json_new)})
+            cur.execute("insert into storage values(:p, CURRENT_TIMESTAMP, 'A', :c)", {'p': id, 'c': json.dumps(json_new)})
             cur.close()
 
         return json_new
@@ -310,6 +312,7 @@ class JSONStorage:
             status.HTTP_200_OK: RESPONSE_JSON,
             **responses(status.HTTP_404_NOT_FOUND, status.HTTP_501_NOT_IMPLEMENTED)
         },
+        'name': 'storage.patch',
         'summary': 'Change a stored json object',
         'description': 'For json patch syntax, see [http://jsonpatch.com/](http://jsonpatch.com/).',
         'response_description': 'The stored json object'
@@ -322,23 +325,32 @@ class JSONStorage:
         found = 0
         with self._db_connect() as conn:
             cur = conn.cursor()
-            cur.execute("delete from storage where name = :p", {'p': id})
-            found = cur.rowcount
+
+            cur.execute("select 1 from storage s where s.name = :p and not exists (select 1 from storage where name = s.name and effdt > s.effdt) and s.status = 'A'", {'p': id})
+            row = cur.fetchone()
+            if row is None:
+                raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+            #cur.execute("delete from storage where name = :p", {'p': id})
+            cur.execute("insert into storage values(:p, CURRENT_TIMESTAMP, 'I', '')", {'p': id})
             cur.close()
         # default satus 204 when deleting something, 404 when there was nothing to delete
-        if found < 1:
-            raise HTTPException(status.HTTP_404_NOT_FOUND)
     delete_json.route = '/{id:path}'
     delete_json.route_params = {
         'response_class': Response,
         'responses': responses(status.HTTP_204_NO_CONTENT, status.HTTP_404_NOT_FOUND),
         'status_code': status.HTTP_204_NO_CONTENT,
+        'name': 'storage.delete',
         'summary': 'Remove a stored json object',
         'description': TrueString()
     }
 
     def router(self):
-        """Create routings"""
+        """Create routings from methods"""
+        # .route = '...'        - FastAPI route
+        # .route_params = {}    - other route parameters (kwargs)
+        # .httpmethod = '...'   - (optional) http method (GET, POST, PUT, PATCH, DELETE, HEAD)
+        #                         otherwize, use the first part of the name (up until the first _)
         handlers = []
         for name in dir(self):
             # quickly skip special methods
@@ -388,8 +400,8 @@ class JSONStorage:
         """Initialize the database"""
         with self._db_connect() as conn:
             conn.executescript("""
-            create table storage (name text, content text);
-            create unique index idx_storage on storage (name);
+            create table storage (name text, effdt datetime, status text, content text);
+            create unique index idx_storage on storage (name, effdt);
             """)
 
 
@@ -420,18 +432,18 @@ def main(args=None):
     # Password
     pwd = cfg.get('security.password')
     if pwd is not None:
-        cfg.set('security.hash', self.context.hash(pwd))
-        cfg.delete('security.password')
+        cfg['security.hash'] = Security.context.hash(pwd)
+        del cfg['security.password']
         cfg.save()
     pwd_hash = cfg.get('security.hash')
     if pwd_hash is None:
         pwd = getpass.getpass()
-        pwd_hash = self.context.hash(pwd)
-        cfg.set('security.hash', pwd_hash)
+        pwd_hash = Security.context.hash(pwd)
+        cfg['security.hash'] = pwd_hash
         cfg.save()
     del pwd
 
-    security = Security(cfg.get('security.hash'), period=cfg.get('security.period', 0), new_hash=save_new_hash(cfg))
+    security = Security(cfg['security.hash'], period=cfg.get('security.period', 0), new_hash=save_new_hash(cfg))
 
     # JSON Storage
     jsonstore = JSONStorage(base_dir, cfg)
